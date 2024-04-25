2a5eed5d8c8caa02b5a94c0486ce3b314e8c1db4
Zachary Granados <zcbg@umich.edu>
2024-04-25 11:27:53 -0400

    when you start setting up db, it is easier to have setup in seperate file

---
+++ attendance_setup.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+def setup_attendance(cur, conn):
+    pass
+
+    # create attendance table if not exist
+
+    # table has team_id (int) 2023 attendance (double) and 2022 average attendance (double)
