25f2ac57888a5a3823fc3faac1b38bfe2d3df1ee
Zachary Granados <zcbg@umich.edu>
2024-04-28 16:36:57 -0400

    Filtered data to make visualizations more readable

--- attendance_visulizations.py
+++ attendance_visulizations.py
@@ -2,18 +2,23 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 def create_barGraph(attendance_data):
-    
     # Extract team names and growth values from the list of tuples
     teams, growths = zip(*attendance_data)
 
-    teams = list(teams)
-    growths = list(growths)
+    # Sort teams and growths based on growth values
+    sorted_data = sorted(zip(teams, growths), key=lambda x: x[1], reverse=True)
+
+    # Get top ten teams and growths
+    top_teams, top_growths = zip(*sorted_data[:10])
+    top_teams = list(top_teams)
+    top_growths = list(top_growths)
 
     # Create a bar plot using Seaborn
-    sns.barplot(x=growths, y=teams, orient='h')
-    plt.title('Attendance Growth')
-    plt.xlabel('Growth(%)')
+    sns.barplot(x=top_growths, y=top_teams, orient='h')
+    plt.title('Attendance Growth (Top 10)')
+    plt.xlabel('Growth (%)')
     plt.ylabel('Teams')
     # Rotate y-axis labels by 45 degrees for better readability
-    plt.yticks(rotation=45, fontsize=4)
+    plt.yticks(rotation=45, fontsize=8)
     plt.show()
+
--- game_visualizations.py
+++ game_visualizations.py
@@ -2,19 +2,25 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 def create_barGraph(data):
-    # Convert dictionary keys and values into lists
+  # Convert dictionary keys and values into lists
     home_teams = list(data.keys())
     win_rates = list(data.values())
 
-    # Create bar plot using Seaborn
-    sns.barplot(x=win_rates, y=home_teams)
+    # Sort teams and win_rates based on win rates
+    sorted_data = sorted(zip(win_rates, home_teams), reverse=True)
+    top_win_rates, top_home_teams = zip(*sorted_data[:25])
+    top_win_rates = list(top_win_rates)
+    top_home_teams = list(top_home_teams)
+
+    # Create bar plot using Seaborn for the top 25 home win rates
+    sns.barplot(x=list(top_win_rates), y=list(top_home_teams), color = "blue")
     plt.xlabel('Win Rate', fontsize=10)  # Set x-axis label with font size 10
     plt.ylabel('Home Team')
-    plt.title('Home Team Win Rate')
+    plt.title('Top 25 Home Team Win Rates')
 
-    # Set x-axis tick labels font size to 5
+    # Set x-axis tick labels font size to 8
     plt.xticks(fontsize=8)
-    plt.yticks(fontsize=5)
 
     plt.tight_layout()
     plt.show()
+
